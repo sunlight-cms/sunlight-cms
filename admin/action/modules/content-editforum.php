bf2db00a4854eace6152b19753ed11099b5bf36f
Jirka Daněk <jirka.danek@jdanek.eu>
2017-07-24 23:45:01 +0200

    Edit script UI improvements

--- admin/action/modules/content-editforum.php
+++ admin/action/modules/content-editforum.php
@@ -12,13 +12,13 @@ $type = _page_forum;
 require _root . 'admin/action/modules/include/page-editscript-init.php';
 if ($continue) {
     $custom_settings = "
-  <label><input type='checkbox' name='var2' value='1'" . _checkboxActivate($query['var2']) . "> " . _lang('admin.content.form.locked3') . "</label> 
-  <label><input type='checkbox' name='var3' value='1'" . _checkboxActivate($query['var3']) . "> " . _lang('admin.content.form.unregpost') . "</label> 
+  <tr><td colspan='2'><label><input type='checkbox' name='var2' value='1'" . _checkboxActivate($query['var2']) . "> " . _lang('admin.content.form.locked3') . "</label></td></tr> 
+  <tr><td colspan='2'><label><input type='checkbox' name='var3' value='1'" . _checkboxActivate($query['var3']) . "> " . _lang('admin.content.form.unregpost') . "</label></td></tr>
   ";
     if (!$new) {
-        $custom_settings .= " <label><input type='checkbox' name='delposts' value='1'> " . _lang('admin.content.form.deltopics') . "</label><small>(" . DB::count(_posts_table, 'home=' . DB::val($id) . ' AND type=' . _post_forum_topic . ' AND xhome=-1') . ")</small>";
+        $custom_settings .= " <tr><td colspan='2'><label><input type='checkbox' name='delposts' value='1'> " . _lang('admin.content.form.deltopics') . "<small>(" . DB::count(_posts_table, 'home=' . DB::val($id) . ' AND type=' . _post_forum_topic . ' AND xhome=-1') . ")</small></label></td></tr>";
     }
-    $custom_settings .= " <input type='number' min='1' name='var1' value='" . $query['var1'] . "' class='inputmini'> " . _lang('admin.content.form.topicssperpage');
+    $custom_settings .= "<tr><td><input type='number' min='1' name='var1' value='" . $query['var1'] . "' class='inputmax'></td><td>" . _lang('admin.content.form.topicssperpage') . "</td></tr>";
     $custom_save_array = array(
         'var1' => array('type' => 'int', 'nullable' => true),
         'var2' => array('type' => 'bool', 'nullable' => false),
--- admin/action/modules/content-editsection.php
+++ admin/action/modules/content-editsection.php
@@ -18,11 +18,11 @@ if ($continue) {
         'delcomments' => array('type' => 'bool', 'nullable' => true),
     );
     $custom_settings = "
-  <label><input type='checkbox' name='var1' value='1'" . _checkboxActivate($query['var1']) . "> " . _lang('admin.content.form.comments') . "</label> 
-  <label><input type='checkbox' name='var3' value='1'" . _checkboxActivate($query['var3']) . "> " . _lang('admin.content.form.commentslocked') . "</label>
+  <tr><td colspan='2'><label><input type='checkbox' name='var1' value='1'" . _checkboxActivate($query['var1']) . "> " . _lang('admin.content.form.comments') . "</label></td></td></td></tr>
+  <tr><td colspan='2'><label><input type='checkbox' name='var3' value='1'" . _checkboxActivate($query['var3']) . "> " . _lang('admin.content.form.commentslocked') . "</label></td></td></td></tr>
   ";
     if (!$new) {
-        $custom_settings .= " <label><input type='checkbox' name='delcomments' value='1'> " . _lang('admin.content.form.delcomments') . "</label><small>(" . DB::count(_posts_table, 'home=' . DB::val($id) . ' AND type=' . _post_section_comment) . ")</small>";
+        $custom_settings .= "<tr><td colspan='2'><label><input type='checkbox' name='delcomments' value='1'> " . _lang('admin.content.form.delcomments') . " <small>(" . DB::count(_posts_table, 'home=' . DB::val($id) . ' AND type=' . _post_section_comment) . ")</small></label></td></td></td></tr>";
     }
 }
 require _root . 'admin/action/modules/include/page-editscript.php';
