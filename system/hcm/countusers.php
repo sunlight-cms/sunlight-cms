9e522eae6238505e0148cbb82cd770dc708b566e
Jirka Daněk <jirka.danek@jdanek.eu>
2018-06-08 22:59:27 +0200

    Fix HCM countusers condition

--- system/hcm/countusers.php
+++ system/hcm/countusers.php
@@ -9,7 +9,7 @@ return function ($group_id = null) {
     if (isset($group_id)) {
         $cond = Hcm::createColumnInSqlCondition("group_id", $group_id);
     } else {
-        $cond = "";
+        $cond = "1";
     }
 
     return DB::count(_users_table, $cond);
