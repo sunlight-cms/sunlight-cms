f3f35df1cd4fecb7fcea8ddc6cb77b2fd5d2f753
jDanek <jirka.danek@jdanek.eu>
2021-02-12 19:17:13 +0100

    Improve HCM modules

--- system/hcm/linkart.php
+++ system/hcm/linkart.php
@@ -6,7 +6,13 @@ use Sunlight\Router;
 defined('_root') or exit;
 
 return function ($id = null, $text = null, $nove_okno = false) {
-    $query = DB::queryRow('SELECT art.id' . ($text === null ? ',art.title' : '') . ',art.slug,cat.slug AS cat_slug FROM ' . _article_table . ' AS art JOIN ' . _page_table . ' AS cat ON(cat.id=art.home1) WHERE art.' . (is_numeric($id) ? 'id' : 'slug') . '=' . DB::val($id));
+    $is_id = is_numeric($id);
+    if ($is_id) {
+        $id = (int) $id;
+    } else {
+        $id = DB::val($id);
+    }
+    $query = DB::queryRow('SELECT art.id' . ($text === null ? ',art.title' : '') . ',art.slug,cat.slug AS cat_slug FROM ' . _article_table . ' AS art JOIN ' . _page_table . ' AS cat ON(cat.id=art.home1) WHERE art.' . ($is_id ? 'id' : 'slug') . '=' . $id);
 
     if ($query === false) {
         return '{' . _e($id) . '}';
@@ -14,5 +20,5 @@ return function ($id = null, $text = null, $nove_okno = false) {
 
     $text = $text === null ? $query['title'] : _e($text);
 
-    return "<a href='" . Router::article(null, $query['slug'], $query['cat_slug']) . "'" . ($nove_okno ? ' target="_blank"' : '') . ">" . $text . "</a>";
+    return "<a href='" . Router::article($query['id'], $query['slug'], $query['cat_slug']) . "'" . ($nove_okno ? ' target="_blank"' : '') . ">" . $text . "</a>";
 };
