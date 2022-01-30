ca1800db05182708cb49289a92ee4ae7810d4836
ShiraNai7 <shiranai7@hotmail.com>
2022-01-30 13:02:41 +0100

    Devkit - increase default dumper max level and string length

--- plugins/extend/devkit/functions.php
+++ plugins/extend/devkit/functions.php
@@ -4,7 +4,7 @@ use Sunlight\Core;
 use Kuria\Debug\Dumper;
 
 if (!function_exists('dump')) {
-    function dump($value, $maxLevel = Dumper::DEFAULT_MAX_LEVEL, $maxStringLen = Dumper::DEFAULT_MAX_STRING_LENGTH)
+    function dump($value, $maxLevel = Dumper::DEFAULT_MAX_LEVEL + 1, $maxStringLen = Dumper::DEFAULT_MAX_STRING_LENGTH * 2)
     {
         $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
 
@@ -24,7 +24,7 @@ if (!function_exists('dump')) {
     }
 
     if (!function_exists('dd')) {
-        function dd($value, $maxLevel = Dumper::DEFAULT_MAX_LEVEL, $maxStringLen = Dumper::DEFAULT_MAX_STRING_LENGTH)
+        function dd($value, $maxLevel = Dumper::DEFAULT_MAX_LEVEL + 1, $maxStringLen = Dumper::DEFAULT_MAX_STRING_LENGTH * 2)
         {
             echo '<pre>', _e(Dumper::dump($value, $maxLevel, $maxStringLen)), '</pre>';
             exit(1);
