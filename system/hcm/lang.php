c529ef69fa073a0a845554cd4feeb5e4fdf9a01c
ShiraNai7 <shiranai7@hotmail.com>
2022-08-15 20:30:04 +0200

    Add HCM lang

---
+++ system/hcm/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+return function ($key = '', ...$args) {
+    if ($args) {
+        $replacements = [];
+        $argCount = count($args);
+
+        for ($i = 0; $i < $argCount; $i += 2) {
+            $replacements[$args[$i]] = $args[$i + 1] ?? '';
+        }
+    } else {
+        $replacements = null;
+    }
+
+    return _lang($key, $replacements);
+};
