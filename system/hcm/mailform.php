3b9da03ffa0c1a6cc22f807d64bc1c5af1b29ba1
ShiraNai7 <shiranai7@hotmail.com>
2022-08-23 13:19:46 +0200

    Fix more variables

--- system/hcm/mailform.php
+++ system/hcm/mailform.php
@@ -54,7 +54,7 @@ return function ($adresa = '', $predmet = null) {
                 'action' => Router::path('system/script/hcm/mform.php', ['query' => ['_return' => $GLOBALS['_index']->url]]),
             ],
             [
-                ['label' => _lang('hcm.mailform.sender'), 'content' => '<input type="email" class="inputsmall" name="sender" value="' . $sende . '"><input type="hidden" name="fid" value="' . Core::$hcmUid . '">' ],
+                ['label' => _lang('hcm.mailform.sender'), 'content' => '<input type="email" class="inputsmall" name="sender" value="' . $sender . '"><input type="hidden" name="fid" value="' . Core::$hcmUid . '">' ],
                 ['label' => _lang('posts.subject'), 'content' => '<input type="text" class="inputsmall" name="subject"' . $rsubject . '>'],
                 $rcaptcha,
                 ['label' => _lang('hcm.mailform.text'), 'content' => '<textarea class="areasmall" name="text" rows="9" cols="33"></textarea>', 'top' => true],
