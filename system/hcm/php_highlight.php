432608b71f2de63a1132941c0ce0a9a4d669c33f
Shira <1024071+Shira-3749@users.noreply.github.com>
2023-08-02 15:17:35 +0200

    HCM php_highlight - trim, add class argument

--- system/hcm/php_highlight.php
+++ system/hcm/php_highlight.php
@@ -2,8 +2,11 @@
 
 use Sunlight\Hcm;
 
-return function ($php_code = '') {
+return function ($php_code = '', $class = null) {
     Hcm::normalizeArgument($php_code, 'string');
+    Hcm::normalizeArgument($class, 'string', true);
 
-    return '<div class="pre php-source">' . highlight_string($php_code, true) . '</div>';
+    return '<div class="pre php-source' . ($class !== null ? ' ' . _e($class) : '') . '">' 
+        . highlight_string(trim($php_code), true) 
+        . '</div>';
 };
