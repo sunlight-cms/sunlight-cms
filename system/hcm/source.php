79e178205bdf8b9531b356122f8bab8e6da36227
Shira <1024071+Shira-3749@users.noreply.github.com>
2023-08-02 14:55:10 +0200

    Improve HCM source

--- system/hcm/source.php
+++ system/hcm/source.php
@@ -2,8 +2,13 @@
 
 use Sunlight\Hcm;
 
-return function ($code = '') {
-    Hcm::normalizeArgument($code, 'string');
+return function ($content = '', $class = null) {
+    Hcm::normalizeArgument($content, 'string');
+    Hcm::normalizeArgument($class, 'string', true);
 
-    return '<div class="pre">' . nl2br(_e(trim($code)), false) . '</div>';
+    return '<pre'
+        . ($class !== null ? ' class="' . _e($class) . '"' : '')
+        . '>'
+        . _e(trim($content)) 
+        . '</pre>';
 };
