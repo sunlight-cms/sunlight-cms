7d3b7e95f804085e31b432805095931ca265fcf0
Shira <1024071+Shira-3749@users.noreply.github.com>
2023-11-22 14:22:04 +0100

    Fix default content value in postprev.php

--- system/script/postprev.php
+++ system/script/postprev.php
@@ -7,4 +7,4 @@ use Sunlight\Util\Request;
 require __DIR__ . '/../bootstrap.php';
 Core::init();
 
-echo Post::render(_e(Request::post('content'), ''));
+echo Post::render(_e(Request::post('content', '')));
